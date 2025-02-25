87a00bab59cbfa031e097440a64dde5e450487b3
vitor caetano <vitorcaetano505@gmail.com>
2025-02-24 22:52:39 -0300

    s

---
+++ portifolio.js
@@ -0,0 +1,14 @@
+const tec_btn = document.querySelectorAll('.btn_tec');
+const res = document.querySelectorAll('.hidden pre');
+
+console.log(res)
+
+tec_btn.forEach((btn, index) => {
+    btn.addEventListener("mouseover", () =>{
+        res.forEach(pre => {
+            pre.style.display = "none"
+        });
+        res[index].style.display = "block"
+    });
+    
+})
